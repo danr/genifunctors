506da9976b67db96884548d9b9e3e751cc9bd3af
Víctor López Juan <victor@lopezjuan.com>
2014-08-25 17:12:46 +0200

    Custom folds and traversals of subparts.

--- Test.hs
+++ Test.hs
@@ -30,3 +30,18 @@ foldMapEither = $(genFoldMap ''Either)
 
 travTriple :: Applicative f => (a -> f a') -> (b -> f b') -> (c -> f c') -> (a,b,c) -> f (a',b',c')
 travTriple = $(genTraverse ''(,,))
+
+foldTupleRev :: Monoid m => (a -> m) -> (b -> m) -> (a,b) -> m
+foldTupleRev f g (a,b) = g b <> f a
+
+foldUCustom :: Monoid m => (a -> m) -> (b -> m) -> (c -> m) -> (d -> m) -> U a b c d -> m
+foldUCustom = $(genFoldMapT [(''(,), 'foldTupleRev)] ''U)
+
+travTupleRev :: Applicative f => (a -> f a') -> (b -> f b') -> (a,b) -> f (a',b')
+travTupleRev f g (a,b) = (\b a -> (a,b)) <$> g b <*> f a
+
+travUCustom :: Applicative f => (a -> f a') -> (b -> f b') -> (c -> f c') -> (d -> f d') -> U a b c d -> f (U a' b' c' d')
+travUCustom = $(genTraverseT [(''(,), 'travTupleRev), (''V, 'travVCustom)] ''U)
+
+travVCustom :: Applicative f => (a -> f a') -> (b -> f b') -> V a b -> f (V a' b')
+travVCustom = $(genTraverseT [(''U, 'travUCustom)] ''V)
