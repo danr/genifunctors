7a1400f4a3eb14c75e6320081b8f1527817cf3ba
Dan Rosén <danr@chalmers.se>
2014-08-26 11:31:14 +0200

    Add a few unit tests

--- Test.hs
+++ Test.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Test where
+module Main where
 
 import Data.Generics.Genifunctors
 import Data.Monoid
 import Control.Applicative
+import System.Exit
 
 import TestTypes
 
+import Control.Monad.Writer
 
 fmapU :: (a -> a') -> (b -> b') -> (c -> c') -> (d -> d') -> U a b c d -> U a' b' c' d'
 fmapU = $(genFmap ''U)
@@ -40,3 +42,22 @@ travUCustom = $(genTraverseT [(''(,), 'travTupleRev), (''V, 'travVCustom)] ''U)
 
 travVCustom :: Applicative f => (a -> f a') -> (b -> f b') -> V a b -> f (V a' b')
 travVCustom = $(genTraverseT [(''U, 'travUCustom)] ''V)
+
+assertEq :: (Show a,Eq a) => a -> a -> IO ()
+assertEq a b | a == b = return ()
+assertEq a b = do
+  putStrLn $ show a ++ " /=\n" ++ show b
+  exitFailure
+
+main :: IO ()
+main = do
+    let v = L [0 :+: 1, M (X ((Right 5) :+: 4)), M (Z (2,3)), R 6 ""]
+    assertEq (fmapU (+1) (+2) (+3) (+4) v) (L [1 :+: 1, M (X ((Right 9) :+: 4)), M (Z (3,5)), R 9 ""])
+    let s = (:[])
+    assertEq (foldU s s s s v) ([0,5,2,3,6])
+    assertEq (foldUCustom s s s s v) ([0,5,3,2,6])
+    let t = tell . s
+    assertEq (execWriter (travU t t t t v)) ([0,5,2,3,6])
+    assertEq (execWriter (travUCustom t t t t v)) ([0,5,3,2,6])
+
+
--- TestTypes.hs
+++ TestTypes.hs
@@ -5,6 +5,8 @@ data U a b c d
     | M (V (a,b) (Either c d))    -- mutually recursive
     | a :+: Int                   -- infix syntax, record syntax, type synonyms
     | R { c :: c, d :: String }   -- and primitive data types supported
+ deriving (Eq,Show)
 
 data V u v = X (U v v u u) | Z u
+ deriving (Eq,Show)
 
